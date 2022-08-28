ae12b1dee65fd0cbd4cdbaeec03f6e44e743b512
milos <milos_divljak@live.com>
2022-08-28 04:59:17 +0200

    added BasePage and CommonMethods

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import methods.CommonMethods;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage extends CommonMethods {
+    WebDriver driver;
+
+
+    public BasePage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
